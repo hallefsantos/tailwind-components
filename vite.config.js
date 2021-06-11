6a5b6658a9eca22160e2d740eaac84a4e88f1fc7
Hallef Santos <hallefsantos@gmail.com>
2021-06-11 18:50:37 -0300

    WIP

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+const { resolve } = require('path')
+
+module.exports = {
+   root: 'src',
+   build: {
+      outDir: '../dist',
+      rollupOptions: {
+         input: {
+            main: resolve(__dirname, 'src/index.html'),
+            blogPost: resolve(__dirname, 'src/components/blog-post.html'),
+            newsletterSignUp: resolve(__dirname, 'src/components/newsletter-sign-up.html'),
+            popup: resolve(__dirname, 'src/components/popup.html'),
+            select: resolve(__dirname, 'src/components/select.html')
+         },
+      }
+   }
+}
