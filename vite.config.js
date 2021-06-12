002290566db9c4668e7ee6cd7bbe775bb24f52a9
Hallef Santos <hallefsantos@gmail.com>
2021-06-11 23:24:17 -0300

    Add password visibility to production

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,8 @@ module.exports = {
             blogPost: resolve(__dirname, 'src/components/blog-post.html'),
             newsletterSignUp: resolve(__dirname, 'src/components/newsletter-sign-up.html'),
             popup: resolve(__dirname, 'src/components/popup.html'),
-            select: resolve(__dirname, 'src/components/select.html')
+            select: resolve(__dirname, 'src/components/select.html'),
+            passwordVisilibity: resolve(__dirname, 'src/components/password-visibility.html'),
          },
       }
    }
